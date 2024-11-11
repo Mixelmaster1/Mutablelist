35c67a3eb8124b4f09d5f87d70f9357c7100e3da
Xavier Jones <jonesxav002@warren.k12.in.us>
2024-11-11 12:18:04 -0500

    Initial commit

---
+++ app/src/main/java/com/example/mutablelist/MainActivity.kt
@@ -0,0 +1,15 @@
+fun main () {
+    val element mutableListOf()
+    "Ice" to "Water"
+    "Grass" to "Earth"
+    "Magma" to "Fire"
+
+
+    element["Ice"] = "Water";
+    val stateCapitals
+    stateCapitals["Fire"] = "Magma";
+    stateCapitals.remove(key:"Lava")
+    println(stateCapitals["Ice"])
+    println(stateCapitals["Fire"]);
+
+}
